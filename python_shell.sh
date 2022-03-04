8a0a9f885fa9b8de6a7f949741be32fa969505be
franceme <9311496+franceme@users.noreply.github.com>
2022-03-04 10:49:41 -0500

    Update

--- python_shell.sh
+++ python_shell.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install wget -y
+
 wget https://github.com/xonsh/xonsh/releases/latest/download/xonsh-x86_64.AppImage
 chmod +x xonsh-x86_64.AppImage
 sudo mv xonsh-x86_64.AppImage /bin/xonsh
