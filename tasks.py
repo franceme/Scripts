3c3c327a84467f2587197bf6d9c796652dd2e94b
franceme <9311496+franceme@users.noreply.github.com>
2022-05-31 13:34:51 -0400

    Update

--- tasks.py
+++ tasks.py
@@ -3,7 +3,10 @@ import os,sys
 
 if __name__ == "__main__":
 	os.system(f"{sys.executable} -m pip install --upgrade invoke funbelts")
-	return
+	if os.path.exists("~/.bashrc"):
+		with open("~/.bashrc", "a") as appender:
+			appender.write("alias voke=invoke")
+	sys.exit(0)
 
 from invoke import task
 import funbelts as ut
